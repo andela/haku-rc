503d0ae0999bd568ac0461c277acae44cc541d5a
Andela Developer <tawakalt.olaniyi@andela.com>
2017-08-14 19:13:12 +0100

    feature(real-time-search): write e2e tests for the real-time search
    Finishes #149303915

---
+++ tests/acceptance-tests/test/specs/smoke-tests/search-app-test.js
@@ -0,0 +1,49 @@
+"use strict";
+const yaml = require("js-yaml");
+const fs   = require("fs");
+const expect = require("chai").expect;
+
+beforeEach(function () {
+  const browserConfig = yaml.safeLoad(fs.readFileSync("./tests/acceptance-tests/config/settings.yml", "utf8"));
+  const baseUrl = browserConfig.base_url.toString();
+  browser.url(baseUrl);
+});
+
+describe("Real Time Search", function () {
+  it("should display all three products that meet the search criteria", function () {
+    const eleMap = yaml.safeLoad(fs.readFileSync("./tests/acceptance-tests/elements/element-map.yml", "utf8"));
+    const inputText = "e";
+
+    browser.pause("5000");
+    browser.click(eleMap.search_test);
+    browser.pause(5000);
+    browser.setValue("#search-input", inputText);
+    browser.pause(5000);
+    expect(browser.getText("#suggestedTitle")).to.contain("BASIC REACTION PRODUCT");
+    expect(browser.getText("#suggestedTitle")).to.contain("BETTER NAME");
+    expect(browser.getText("#suggestedTitle")).to.contain("ADETOKUNBO");
+  });
+  it("should display all two products that meet the search criteria", function () {
+    const eleMap = yaml.safeLoad(fs.readFileSync("./tests/acceptance-tests/elements/element-map.yml", "utf8"));
+    const inputText = "et";
+
+    browser.pause("5000");
+    browser.click(eleMap.search_test);
+    browser.pause(5000);
+    browser.setValue("#search-input", inputText);
+    browser.pause(5000);
+    expect(browser.getText("#suggestedTitle")).to.contain("BETTER NAME");
+    expect(browser.getText("#suggestedTitle")).to.contain("ADETOKUNBO");
+  });
+  it("should display the only product that meet the search criteria", function () {
+    const eleMap = yaml.safeLoad(fs.readFileSync("./tests/acceptance-tests/elements/element-map.yml", "utf8"));
+    const inputText = "ett";
+
+    browser.pause("5000");
+    browser.click(eleMap.search_test);
+    browser.pause(5000);
+    browser.setValue("#search-input", inputText);
+    browser.pause(5000);
+    expect(browser.getText("#suggestedTitle")).to.equal("BETTER NAME");
+  });
+});
